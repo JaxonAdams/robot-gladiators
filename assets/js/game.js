fc9464dff525958579bec662f921eac2bd051df8
Jaxon Adams <jadamsresume1@gmail.com>
2022-01-21 14:54:26 -0700

    set up enemy name array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+//Game States
+//"WIN" - Player robot has defeated all enemy robots
+//      *Fight all enemy robots
+//      *Defeat each enemy robot
+//"LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -5,11 +11,17 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Mr. Roboto";
+var enemyNames = ["Mr. Roboto", "Amy Android", "Paige"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyAttack, enemyHealth);
+console.log(enemyNames, enemyAttack, enemyHealth);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 
 var fight = function() {
     // Alert players that they are starting the round.
@@ -77,4 +89,4 @@ var fight = function() {
     }
 };
 
-fight();
+// fight();
