ed33a870b45395865988f307902d77f94565545e
Jaxon Adams <jadamsresume1@gmail.com>
2022-01-24 15:13:30 -0700

    wrapping up MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,16 +74,52 @@ var fight = function(enemyName) {
     }
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+    
+        else {
+            window.alert("Your robot has been defeated! Game Over!");
+            break;
+        }
     }
 
-    else {
-        window.alert("Your robot has been defeated! Game Over!");
-        break;
+    var endGame = function() {
+        // if the player is still alive, they win
+        if (playerHealth > 0) {
+            window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + " .");
+        }
+
+        else {
+            window.alert("You've lost your robot in battle.");
+        }
+
+        // ask the player if they'd like to play again
+
+        var playAgainConfirm = window.confirm("Would you like to play again?");
+
+        if (playAgainConfirm) {
+            // restart the game
+            startGame();
+        }
+
+        else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        }
     }
+
+    // play again
+    endGame();
 }
+
+startGame();
